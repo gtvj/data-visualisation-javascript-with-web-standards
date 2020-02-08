00e1933b8cf2dc97018ce73a8aed39cea7b39be2
Gwyn Jones <m@gwnjns.com>
2020-02-08 14:06:31 +0000

    Add basic SVG example

--- app/routes.py
+++ app/routes.py
@@ -8,3 +8,8 @@ def home():
     with open('app/data/pages.json') as pages:
         pages = json.load(pages)
     return render_template("index.html", title="Data visualisation in JavaScript", pages=pages)
+
+# All requests for pages go to this route
+@app.route('/pages/<page>')
+def page(page):
+    return render_template("pages/{}.html".format(page))
